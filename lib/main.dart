dd1b8f4c53d76520164d60749688e6e78d9e24ab
Aokiti <yuukiyuuki223@gmail.com>
2021-12-12 17:31:51 +0900

    縦書き時の記号に関する処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:untitled/vertical_rotated.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +35,8 @@ class _MyHomePage extends StatelessWidget {
           return CustomPaint(
             size: Size(constraints.maxWidth, constraints.maxHeight - 32),
             painter: _MyPainter(
-                "吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。"),
+                text:
+                    "「吾輩は猫である。名前-−はまだ無い。」どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。"),
           );
         },
       ),
@@ -41,39 +45,86 @@ class _MyHomePage extends StatelessWidget {
 }
 
 class _MyPainter extends CustomPainter {
-  _MyPainter(this.text);
+  _MyPainter({required this.text, this.fontSize = 24, this.space = 12});
 
   final String text;
+  final double fontSize;
+  final int space;
 
   @override
   void paint(Canvas canvas, Size size) {
     canvas.save();
 
-    const fontSize = 24;
-
-    final rowCount = text.length * fontSize ~/ size.height;
     final columnCount = size.height ~/ fontSize;
-    final runes = text.runes;
+    final rowCount = (text.length / columnCount).ceil();
+
+    print("$rowCount $columnCount ${(text.length * fontSize) % size.height}");
+
+    // print(String.fromCharCode(0x3063));
 
     for (int x = 0; x < rowCount; x++) {
-      for (int y = 0; y < columnCount; y++) {
-        TextSpan span = TextSpan(
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-          ),
-          text: String.fromCharCode(runes.elementAt(x * columnCount + y)),
-        );
-        TextPainter tp = TextPainter(
-          text: span,
-          textDirection: TextDirection.ltr,
-        );
-        tp.layout();
-        tp.paint(canvas,
-            Offset((x * fontSize).toDouble(), (y * fontSize).toDouble()));
-      }
+      drawTextLine(canvas, size, x, columnCount);
     }
+
     canvas.restore();
+  }
+
+  void drawTextLine(Canvas canvas, Size size, int x, int columnCount) {
+    final runes = text.runes;
+    final charWidth = fontSize + space;
+
+    for (int y = 0; y < columnCount; y++) {
+      final charIndex = x * columnCount + y;
+      if (runes.length <= charIndex) return;
+
+      String char = String.fromCharCode(runes.elementAt(charIndex));
+      if (VerticalRotated.map[char] != null) {
+        char = VerticalRotated.map[char] ?? "";
+      }
+
+      // print("0x" + runes.elementAt(x * columnCount + y).toRadixString(16));
+      TextSpan span = TextSpan(
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: fontSize,
+        ),
+        text: char,
+      );
+      TextPainter tp = TextPainter(
+        text: span,
+        textDirection: TextDirection.ltr,
+      );
+
+      tp.layout();
+
+      // if (VerticalRotated.r90degree.contains(char)) {
+      //   canvas.save();
+      //   canvas.translate(
+      //     (size.width - charWidth - x * charWidth).toDouble(),
+      //     (y * fontSize).toDouble(),
+      //   );
+      //   canvas.rotate(pi / 2);
+      //   tp.paint(
+      //     canvas,
+      //     Offset(fontSize / 2, -(fontSize + space / 2)),
+      //     // const Offset(0, 0),
+      //     // Offset(
+      //     //   (size.width - charWidth - x * charWidth).toDouble(),
+      //     //   (y * fontSize).toDouble(),
+      //     // ),
+      //   );
+      //   canvas.restore();
+      // } else {
+
+      tp.paint(
+        canvas,
+        Offset(
+          (size.width - charWidth - x * charWidth).toDouble(),
+          (y * fontSize).toDouble(),
+        ),
+      );
+      // }
+    }
   }
 
   @override
